1fbd86d31a901b7a3c98f4f48f6aefd8eaaed1c8
mcneilljamie <131818783+mcneilljamie@users.noreply.github.com>
2025-12-06 05:17:25 +0000

    Updated JammAMM.sol

--- src/contracts/JammAMM.sol
+++ src/contracts/JammAMM.sol
@@ -8,10 +8,19 @@ interface IERC20 {
 }
 
 contract JammAMM {
+    error ZeroAddress();
+    error ZeroAmount();
+    error InsufficientLiquidity();
+    error SlippageExceeded();
+    error TransferFailed();
+    error ReentrancyDetected();
+    error InvalidLiquidityAmount();
+
     address public token;
     address public constant feeRecipient = 0x227D5F29bAb4Cec30f511169886b86fAeF61C6bc;
     uint256 public constant FEE_PERCENT = 4;
     uint256 public constant FEE_DENOMINATOR = 1000;
+    uint256 public constant MINIMUM_LIQUIDITY = 1000;
 
     uint256 public reserveToken;
     uint256 public reserveETH;
@@ -19,31 +28,49 @@ contract JammAMM {
 
     mapping(address => uint256) public liquidity;
 
+    uint256 private locked = 1;
+
+    modifier nonReentrant() {
+        if (locked != 1) revert ReentrancyDetected();
+        locked = 2;
+        _;
+        locked = 1;
+    }
+
     event LiquidityAdded(address indexed provider, uint256 ethAmount, uint256 tokenAmount, uint256 liquidityMinted);
     event LiquidityRemoved(address indexed provider, uint256 ethAmount, uint256 tokenAmount, uint256 liquidityBurned);
     event Swap(address indexed user, uint256 ethIn, uint256 tokenIn, uint256 ethOut, uint256 tokenOut);
 
     constructor(address _token) {
+        if (_token == address(0)) revert ZeroAddress();
         token = _token;
     }
 
-    function addLiquidity(uint256 tokenAmount) external payable returns (uint256 liquidityMinted) {
-        require(msg.value > 0 && tokenAmount > 0, "Invalid amounts");
+    function addLiquidity(uint256 tokenAmount) external payable nonReentrant returns (uint256 liquidityMinted) {
+        if (msg.value == 0 || tokenAmount == 0) revert ZeroAmount();
 
         if (totalLiquidity == 0) {
             liquidityMinted = msg.value;
+            if (liquidityMinted <= MINIMUM_LIQUIDITY) revert InvalidLiquidityAmount();
+
             reserveETH = msg.value;
             reserveToken = tokenAmount;
+
+            liquidity[address(0)] = MINIMUM_LIQUIDITY;
+            totalLiquidity = MINIMUM_LIQUIDITY;
+            liquidityMinted -= MINIMUM_LIQUIDITY;
         } else {
             uint256 ethLiquidity = (msg.value * totalLiquidity) / reserveETH;
             uint256 tokenLiquidity = (tokenAmount * totalLiquidity) / reserveToken;
             liquidityMinted = ethLiquidity < tokenLiquidity ? ethLiquidity : tokenLiquidity;
 
+            if (liquidityMinted == 0) revert InvalidLiquidityAmount();
+
             reserveETH += msg.value;
             reserveToken += tokenAmount;
         }
 
-        require(IERC20(token).transferFrom(msg.sender, address(this), tokenAmount), "Token transfer failed");
+        if (!IERC20(token).transferFrom(msg.sender, address(this), tokenAmount)) revert TransferFailed();
 
         liquidity[msg.sender] += liquidityMinted;
         totalLiquidity += liquidityMinted;
@@ -53,62 +80,76 @@ contract JammAMM {
         return liquidityMinted;
     }
 
-    function removeLiquidity(uint256 liquidityAmount) external returns (uint256 ethAmount, uint256 tokenAmount) {
-        require(liquidity[msg.sender] >= liquidityAmount, "Insufficient liquidity");
+    function removeLiquidity(uint256 liquidityAmount) external nonReentrant returns (uint256 ethAmount, uint256 tokenAmount) {
+        if (liquidityAmount == 0) revert ZeroAmount();
+        if (liquidity[msg.sender] < liquidityAmount) revert InsufficientLiquidity();
+        if (totalLiquidity == 0) revert InsufficientLiquidity();
 
         ethAmount = (liquidityAmount * reserveETH) / totalLiquidity;
         tokenAmount = (liquidityAmount * reserveToken) / totalLiquidity;
+
+        if (ethAmount == 0 || tokenAmount == 0) revert InvalidLiquidityAmount();
 
         liquidity[msg.sender] -= liquidityAmount;
         totalLiquidity -= liquidityAmount;
         reserveETH -= ethAmount;
         reserveToken -= tokenAmount;
 
-        require(IERC20(token).transfer(msg.sender, tokenAmount), "Token transfer failed");
-        payable(msg.sender).transfer(ethAmount);
+        if (!IERC20(token).transfer(msg.sender, tokenAmount)) revert TransferFailed();
+
+        (bool success, ) = payable(msg.sender).call{value: ethAmount}("");
+        if (!success) revert TransferFailed();
 
         emit LiquidityRemoved(msg.sender, ethAmount, tokenAmount, liquidityAmount);
 
         return (ethAmount, tokenAmount);
     }
 
-    function swapETHForToken(uint256 minTokenOut) external payable returns (uint256 tokenOut) {
-        require(msg.value > 0, "Invalid ETH amount");
+    function swapETHForToken(uint256 minTokenOut) external payable nonReentrant returns (uint256 tokenOut) {
+        if (msg.value == 0) revert ZeroAmount();
+        if (reserveToken == 0 || reserveETH == 0) revert InsufficientLiquidity();
 
         uint256 fee = (msg.value * FEE_PERCENT) / FEE_DENOMINATOR;
         uint256 ethAfterFee = msg.value - fee;
 
         tokenOut = (ethAfterFee * reserveToken) / (reserveETH + ethAfterFee);
-        require(tokenOut >= minTokenOut, "Slippage too high");
-        require(tokenOut <= reserveToken, "Insufficient liquidity");
+        if (tokenOut < minTokenOut) revert SlippageExceeded();
+        if (tokenOut > reserveToken) revert InsufficientLiquidity();
 
         reserveETH += ethAfterFee;
         reserveToken -= tokenOut;
 
-        payable(feeRecipient).transfer(fee);
-        require(IERC20(token).transfer(msg.sender, tokenOut), "Token transfer failed");
+        (bool feeSuccess, ) = payable(feeRecipient).call{value: fee}("");
+        if (!feeSuccess) revert TransferFailed();
+
+        if (!IERC20(token).transfer(msg.sender, tokenOut)) revert TransferFailed();
 
         emit Swap(msg.sender, msg.value, 0, 0, tokenOut);
 
         return tokenOut;
     }
 
-    function swapTokenForETH(uint256 tokenIn, uint256 minETHOut) external returns (uint256 ethOut) {
-        require(tokenIn > 0, "Invalid token amount");
-        require(IERC20(token).transferFrom(msg.sender, address(this), tokenIn), "Token transfer failed");
+    function swapTokenForETH(uint256 tokenIn, uint256 minETHOut) external nonReentrant returns (uint256 ethOut) {
+        if (tokenIn == 0) revert ZeroAmount();
+        if (reserveToken == 0 || reserveETH == 0) revert InsufficientLiquidity();
+
+        if (!IERC20(token).transferFrom(msg.sender, address(this), tokenIn)) revert TransferFailed();
 
         uint256 ethBeforeFee = (tokenIn * reserveETH) / (reserveToken + tokenIn);
         uint256 fee = (ethBeforeFee * FEE_PERCENT) / FEE_DENOMINATOR;
         ethOut = ethBeforeFee - fee;
 
-        require(ethOut >= minETHOut, "Slippage too high");
-        require(ethOut <= reserveETH, "Insufficient liquidity");
+        if (ethOut < minETHOut) revert SlippageExceeded();
+        if (ethOut > reserveETH) revert InsufficientLiquidity();
 
         reserveToken += tokenIn;
         reserveETH -= ethBeforeFee;
 
-        payable(feeRecipient).transfer(fee);
-        payable(msg.sender).transfer(ethOut);
+        (bool feeSuccess, ) = payable(feeRecipient).call{value: fee}("");
+        if (!feeSuccess) revert TransferFailed();
+
+        (bool success, ) = payable(msg.sender).call{value: ethOut}("");
+        if (!success) revert TransferFailed();
 
         emit Swap(msg.sender, 0, tokenIn, ethOut, 0);
 
