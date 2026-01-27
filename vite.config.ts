4c8483632cc46cf7ef14bc5e7bc241fea0bea160
mcneilljamie <131818783+mcneilljamie@users.noreply.github.com>
2026-01-27 20:43:00 +0000

    Added netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
